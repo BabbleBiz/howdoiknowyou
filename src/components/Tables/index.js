0f6c5850aad1efe6b86a00c54487cf4ac38560e7
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-07-09 15:26:29 -0500

    seperating high and low priority

--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -8,20 +8,35 @@ function Tables () {
 
   const db = firebase.firestore();
 
+
   useEffect( () => {
       return db.collection('guests').onSnapshot(snapshot => {
           setGuests( snapshot.docs.map(doc => doc.data()) )
       }, err => {
         console.log(`Encountered error: ${err}`);
       })
+
   }, [])
 
+  let highPrio = guests.filter(guest => guest.priority > 2)
+  let lowPrio = guests.filter(guest => guest.priority <= 2)
     return (
     <div>
       <h1>Tables</h1>
-    {guests.map(guest =>{
-     return (<div key={guest.guestName}>{guest.guestName}</div>)}
-    )}
+      <div>High
+          {highPrio.map(guest => {
+            return (<div key={guest.guestName} >{guest.guestName}</div>)
+          }
+          )}
+      </div>
+      <br/>
+      <div>Low
+          {lowPrio.map(guest => {
+            return (<div key={guest.guestName} >{guest.guestName}</div>)
+          }
+          )}
+        </div>
+
     </div>
     )
 
